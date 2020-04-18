10067dfd288e010a8d17c9db5192b4fded7d321c
Andrius Gliožeris <30297545+nDRIUZ@users.noreply.github.com>
2020-04-18 15:27:15 +0100

    Add project files.

---
+++ MyPortfolioWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyPortfolioWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
